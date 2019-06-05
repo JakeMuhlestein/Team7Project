94347e74371833abba19055def5512b2cfc384bf
Mark Tobler <marktobler@gmail.com>
2019-06-04 19:56:39 -0700

    Mark Tobler avtivity comment

--- app/src/main/java/edu/byui/myapplication/MainActivity.java
+++ app/src/main/java/edu/byui/myapplication/MainActivity.java
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//Mark Tobler comment
 public class MainActivity extends AppCompatActivity {
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
