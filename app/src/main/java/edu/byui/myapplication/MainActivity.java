e0a5c22124e4336418939717f7f9e95f2c0eaafd
chloehuang18 <hua17002@byui.edu>
2019-06-05 19:02:09 -0600

    chloe added a comment

--- app/src/main/java/edu/byui/myapplication/MainActivity.java
+++ app/src/main/java/edu/byui/myapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Govert Carreño Comment
         //Second comment
+        //chloeeee 
     }
 }
