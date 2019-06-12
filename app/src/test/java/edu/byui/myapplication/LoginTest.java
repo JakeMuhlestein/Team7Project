2b7156be2bdcd3695c257f2992fb0ef3d19490e4
chloehuang18 <hua17002@byui.edu>
2019-06-11 21:04:31 -0600

    week8 team activity

---
+++ app/src/test/java/edu/byui/myapplication/LoginTest.java
@@ -0,0 +1,13 @@
+package edu.byui.myapplication;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class LoginTest {
+    @Test
+    public void assertTruth(){
+        assert(true);
+    }
+
+}
