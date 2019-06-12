253a751a264355d140f6bf0a7a1b110c6ddb6cab
Govert Carreno <car16116@byui.edu>
2019-06-11 22:27:32 -0500

    BudgetPresenterTest

---
+++ app/src/main/java/edu/byui/myapplication/presenter/BudgetPresenter.java
@@ -0,0 +1,7 @@
+package edu.byui.myapplication.presenter;
+
+public class BudgetPresenter {
+    public boolean updateCategory(String name, double amount) {
+        return true;
+    }
+}
---
+++ app/src/test/java/edu/byui/myapplication/BudgetPresenterTest.java
@@ -0,0 +1,13 @@
+package edu.byui.myapplication;
+
+import org.junit.Test;
+
+import edu.byui.myapplication.presenter.BudgetPresenter;
+
+public class BudgetPresenterTest {
+    @Test
+    public void createNewUserTest() {
+        BudgetPresenter presenter = new BudgetPresenter();
+        assert(presenter.updateCategory("test",000) );
+    }
+}
