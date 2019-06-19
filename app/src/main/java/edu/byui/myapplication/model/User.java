edf05f247fb3cd5a56c7b6fba426d6458e535385
chloehuang18 <47125676+chloehuang18@users.noreply.github.com>
2019-06-18 20:24:52 -0600

    user and login class

--- app/src/main/java/edu/byui/myapplication/model/User.java
+++ app/src/main/java/edu/byui/myapplication/model/User.java
@@ -1,5 +1,76 @@
 package edu.byui.myapplication.model;
 
-public class User {
+import androidx.appcompat.app.AppCompatActivity;
 
+import java.security.KeyStore;
+
+public class User extends AppCompatActivity {
+    int userID;
+    String username;
+    String password;
+    String diaplayName;
+    String email;
+    String phone;
+    String address;
+    String bday;
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDiaplayName() {
+        return diaplayName;
+    }
+
+    public void setDiaplayName(String diaplayName) {
+        this.diaplayName = diaplayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBday() {
+        return bday;
+    }
+
+    public void setBday(String bday) {
+        this.bday = bday;
+    }
 }
