d1b42836173a3466d35c0a674b7536561b24742f
Govert Carreno <car16116@byui.edu>
2019-06-18 23:35:24 -0500

    LoginActivity xml tools:context changed to point to Login (it was  pointing to MainActivity) Create default constructor and constructor for user Main activite creates an intend of login to call when run Included Login into android manifest Created method to be called by login to save the user into shared preferences Created method to serialize the user object.
    Build.gradle implements GSON library

--- app/src/main/java/edu/byui/myapplication/MainActivity.java
+++ app/src/main/java/edu/byui/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import edu.byui.myapplication.view.Login;
+
 //Mark Tobler comment
 public class MainActivity extends AppCompatActivity {
     //
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Intent intent = new Intent(this,.class);
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/byui/myapplication/view/Login.java
+++ app/src/main/java/edu/byui/myapplication/view/Login.java
@@ -1,19 +1,65 @@
 package edu.byui.myapplication.view;
 
-import android.os.Bundle;
-import android.widget.EditText;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Login  extends AppCompatActivity {
-    EditText mTextUsername;
-    EditText mTextPassword;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import edu.byui.myapplication.R;
+import edu.byui.myapplication.model.User;
+
+public class Login extends AppCompatActivity {
+
+    public final String USER_KEY = "edu.byu.myapplication.USER_KEY";
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 
+    protected void onPause() {
+        super.onPause();
+    }
 
+    private void saveSharedPrederences(String json) {
+
+        //Get the user preferences, or create if not created
+        SharedPreferences sharedPreferences = getApplicationContext().
+                getSharedPreferences("UserPreferences", Context.MODE_PRIVATE);
+        //Because we need to write we create an editor of the preferences got in line 43
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //Set the preference
+        editor.putString(USER_KEY,json);
+
+        //Apply changes into the preferences (This is the part that really saves the data)
+        editor.apply();
+
+        Toast.makeText(getApplicationContext(),
+                "User saved",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void logIn(View view) {
+        Gson g = new Gson();
+
+        //Create the new user (This is stub)
+        User user = new User("Team7",
+                "The team seven",
+                "team7@team7.org",
+                "30023107254",
+                "This is the address");
+        //Create JSON syntax
+        String jsonUser = g.toJson(user);
+
+        //Save preferences
+        saveSharedPrederences(jsonUser);
     }
 }
