7e57bbe6a93519883e65738075eaac43d96f8166
Govert Carreno <car16116@byui.edu>
2019-06-18 21:45:20 -0500

    ViewFolder

--- app/src/main/java/edu/byui/myapplication/MainActivity.java
+++ app/src/main/java/edu/byui/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,.class);
+        //Intent intent = new Intent(this,.class);
     }
 }
