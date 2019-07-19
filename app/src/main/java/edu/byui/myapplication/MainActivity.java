6d66e1ddb8757933ebd5d8e033562a4d1532722f
chloehuang18 <47125676+chloehuang18@users.noreply.github.com>
2019-07-19 13:05:21 -0600

    Update MainActivity.java
    login functions are in main

--- app/src/main/java/edu/byui/myapplication/MainActivity.java
+++ app/src/main/java/edu/byui/myapplication/MainActivity.java
@@ -6,10 +6,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Parcelable;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.facebook.stetho.Stetho;
@@ -18,8 +25,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.List;
 
 import edu.byui.myapplication.model.TeamDatabase;
+import edu.byui.myapplication.model.User;
+import edu.byui.myapplication.model.UserDao;
 import edu.byui.myapplication.model.Vendor;
 import edu.byui.myapplication.view.MenuActivity;
+import edu.byui.myapplication.view.SignUpActivity;
 import edu.byui.myapplication.viewModel.AddVendorActivity;
 import edu.byui.myapplication.viewModel.VendorAdapter;
 import edu.byui.myapplication.viewModel.VendorViewModel;
@@ -31,13 +41,78 @@ public class MainActivity extends AppCompatActivity {
 
     private VendorViewModel vendorViewModel;
 
+    private EditText edtUsername;
+    private EditText edtPassword;
+    private TeamDatabase database;
+
+    private Button btLogin;
+    private Button btSignUp;
+
+    private UserDao userDao;
+    private ProgressDialog progressDialog;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-        startActivityForResult(intent, 1);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Login...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setProgress(0);
+
+        database = Room.databaseBuilder(this, TeamDatabase.class, "mi-database.db")
+                .allowMainThreadQueries()
+                .build();
+        userDao = database.getUserDao();
+
+        btLogin = findViewById(R.id.login);
+        btSignUp = findViewById(R.id.register);
+
+        edtUsername = findViewById(R.id.logUsername);
+        edtPassword = findViewById(R.id.logpassword);
+
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!emptyValidation()) {
+                    progressDialog.show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            User user = userDao.getUser(edtUsername.getText().toString(), edtPassword.getText().toString());
+                            if(user!=null){
+                                Intent i = new Intent(MainActivity.this, MenuActivity.class);
+                                i.putExtra("User", (Parcelable) user);
+                                startActivity(i);
+                                finish();
+                            }else{
+                                Toast.makeText(MainActivity.this, "Unregistered user, or incorrect", Toast.LENGTH_SHORT).show();
+                            }
+                            progressDialog.dismiss();
+                        }
+                    }, 1000);
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Empty Fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        //Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+        //startActivityForResult(intent, 1);
         /*
         FloatingActionButton buttonAddVendor = findViewById(R.id.button_add_vendor);
         buttonAddVendor.setOnClickListener(new View.OnClickListener() {
@@ -82,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         */
 
@@ -112,4 +187,15 @@ public class MainActivity extends AppCompatActivity {
     */
 
     //TODO: onResume needs to include a call to DataRepository's doNothing() method
+
+    private boolean emptyValidation() {
+        if (TextUtils.isEmpty(edtUsername.getText().toString()) || TextUtils.isEmpty(edtPassword.getText().toString())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
+
