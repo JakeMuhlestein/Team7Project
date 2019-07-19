c65cd54a187e39b39a1b95b68c79049cd96fd09a
chloehuang18 <47125676+chloehuang18@users.noreply.github.com>
2019-07-19 13:01:01 -0600

    Update UserDao.java
    add query

--- app/src/main/java/edu/byui/myapplication/model/UserDao.java
+++ app/src/main/java/edu/byui/myapplication/model/UserDao.java
@@ -23,8 +23,8 @@ public interface UserDao {
     @Query("SELECT * FROM user WHERE id  = :userId")
     User loadUserById(int userId);
 
-    @Query("SELECT * FROM user WHERE username  = :username")
-    List<User> loadUserByUsername(String username);
+   @Query("SELECT * FROM user WHERE username  = :username and password = :password")
+    User getUser(String username, String password);
 
     @Insert
     void insert(User users);
