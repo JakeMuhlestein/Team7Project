3136343f0e45cffe8e5e261ef6ba6a585d542f52
Mark Tobler <marktobler@gmail.com>
2019-07-20 07:24:40 -0700

    Vehicle and UI Changes: Database ver has changed again to 4!! Vehicle: Changed Year from Date to Int VehicleFragment: Implemented the Activity logic AddEditVehicleActivity - renamed AddVehicleActivity VehicleAdapter: Implemented clickListener Colors: changed colors

--- app/src/main/java/edu/byui/myapplication/view/VehicleFragment.java
+++ app/src/main/java/edu/byui/myapplication/view/VehicleFragment.java
@@ -1,9 +1,7 @@
 package edu.byui.myapplication.view;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +12,7 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +27,7 @@ import java.util.List;
 
 import edu.byui.myapplication.R;
 import edu.byui.myapplication.model.Vehicle;
-import edu.byui.myapplication.viewModel.AddVehicleActivity;
+import edu.byui.myapplication.viewModel.AddEditVehicleActivity;
 import edu.byui.myapplication.viewModel.VehicleAdapter;
 import edu.byui.myapplication.viewModel.VehicleViewModel;
 
@@ -45,9 +44,9 @@ import static edu.byui.myapplication.viewModel.VehicleActivity.EDIT_VEHICLE_REQU
  * create an instance of this fragment.
  */
 public class VehicleFragment extends Fragment {
-    // TODO: I haven't implemented much of this except the skeleton. =/
-    // TODO: Rename parameter arguments, choose names that match
-
+    private final String TAG = "** ** VehicleFragment: ";
+    public static final int ADD_VEHICLE_REQUEST = 1;
+    public static final int EDIT_VEHICLE_REQUEST = 2;
     private VehicleViewModel vehicleViewModel;
 
     @Nullable
@@ -60,9 +59,8 @@ public class VehicleFragment extends Fragment {
         buttonAddVehicle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), AddVehicleActivity.class);
-                // TODO: request code reserarch = 1?
-                startActivityForResult(intent, 1);
+                Intent intent = new Intent(getActivity(), AddEditVehicleActivity.class);
+                startActivityForResult(intent, ADD_VEHICLE_REQUEST);
             }
         });
 
@@ -70,14 +68,35 @@ public class VehicleFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setHasFixedSize(true);
 
+        Log.d(TAG, "onCreate: About to initialize Vehicle Adapter");
         final VehicleAdapter adapter = new VehicleAdapter();
         recyclerView.setAdapter(adapter);
 
         vehicleViewModel = ViewModelProviders.of(getActivity()).get(VehicleViewModel.class);
         vehicleViewModel.getAllVehicles().observe(getViewLifecycleOwner(), new Observer<List<Vehicle>>() {
             @Override
-            public void onChanged(List<Vehicle> vehicles) {
+            public void onChanged(@Nullable List<Vehicle> vehicles) {
                 adapter.setVehicles(vehicles);
+            }
+        });
+
+        Log.d(TAG, "onCreate: right before adapter.setOnItemClickListener");
+        adapter.setOnItemClickListener(new VehicleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Vehicle vehicle) {
+                Log.d(TAG, "adapter.setOnItemClickListener: onItemClick has been fired!");
+                Intent intent = new Intent(getActivity(), AddEditVehicleActivity.class);
+
+                intent.putExtra(AddEditVehicleActivity.EXTRA_ID, vehicle.getId());
+                intent.putExtra(AddEditVehicleActivity.EXTRA_NAME, vehicle.getName());
+                intent.putExtra(AddEditVehicleActivity.EXTRA_AMOUNT, vehicle.getAmount());
+                intent.putExtra(AddEditVehicleActivity.EXTRA_MAKE, vehicle.getMake());
+                intent.putExtra(AddEditVehicleActivity.EXTRA_MODEL, vehicle.getModel());
+                intent.putExtra(AddEditVehicleActivity.EXTRA_YEAR, vehicle.getYear());
+                intent.putExtra(AddEditVehicleActivity.EXTRA_MILEAGE, vehicle.getMiles());
+
+                startActivityForResult(intent, EDIT_VEHICLE_REQUEST);
+
             }
         });
 
@@ -88,56 +107,51 @@ public class VehicleFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            /*
-            String name = data.getStringExtra(AddVehicleActivity.EXTRA_NAME);
+        if(requestCode == ADD_VEHICLE_REQUEST && resultCode == RESULT_OK) {
+            String vehicleName = data.getStringExtra(AddEditVehicleActivity.EXTRA_NAME);
+            String vehicleMake = data.getStringExtra(AddEditVehicleActivity.EXTRA_MAKE);
+            String vehicleModel = data.getStringExtra(AddEditVehicleActivity.EXTRA_MODEL);
+            int vehicleYear = data.getIntExtra(AddEditVehicleActivity.EXTRA_YEAR, 1999);
+            int vehicleMileage = data.getIntExtra(AddEditVehicleActivity.EXTRA_MILEAGE, 0);
 
-            Vehicle vehicle = new Vehicle(name);
-            vehicleViewModel.insert(vehicle);
-            Toast.makeText(getActivity(), "Vehicle saved", Toast.LENGTH_SHORT).show();
-            */
-            if (requestCode == ADD_VEHICLE_REQUEST  && resultCode == RESULT_OK) {
-                String vehicleName = data.getStringExtra(AddVehicleActivity.EXTRA_NAME);
-                String vehicleMake = data.getStringExtra(AddVehicleActivity.EXTRA_MAKE);
-                String vehicleModel = data.getStringExtra(AddVehicleActivity.EXTRA_MODEL);
-                //String tempStr = data.getStringExtra(AddVehicleActivity.EXTRA_YEAR); // TODO: do we need to allow for null?
-                int vehicleYear =  data.getIntExtra(AddVehicleActivity.EXTRA_YEAR, 1999); //  Integer.parseInt(tempStr); //
-                //tempStr = data.getStringExtra(AddVehicleActivity.EXTRA_MILEAGE); // TODO: do we need to allow for null?
-                int vehicleMileage = data.getIntExtra(AddVehicleActivity.EXTRA_MILEAGE, 0);
-
-                //verify
-                double vehicleAmount = data.getDoubleExtra(AddVehicleActivity.EXTRA_AMOUNT, 0.00);
+            //verify
+            double vehicleAmount = Double.parseDouble(data.getStringExtra(AddEditVehicleActivity.EXTRA_AMOUNT));
+                //double vehicleAmount = data.getDoubleExtra(AddEditVehicleActivity.EXTRA_AMOUNT, 0.00);
 
                 // for the Year date
-                Calendar calYear = new GregorianCalendar(vehicleYear, 1, 1);
+                //Calendar calYear = new GregorianCalendar(vehicleYear, 1, 1);
 
-                Vehicle vehicle = new Vehicle(vehicleMake, vehicleModel, new Date(calYear.getTimeInMillis()),
-                        vehicleMileage, vehicleName, vehicleAmount);
-                vehicleViewModel.insertVehicle(vehicle);
-                Toast.makeText(getActivity(), "Vehicle saved", Toast.LENGTH_SHORT).show();
-            } else if(requestCode== EDIT_VEHICLE_REQUEST && resultCode == RESULT_OK){
-                int id = data.getIntExtra(AddVehicleActivity.EXTRA_ID, -1);
+                //Vehicle vehicle = new Vehicle(vehicleMake, vehicleModel, new Date(calYear.getTimeInMillis()), vehicleMileage, vehicleName, vehicleAmount);
+            Vehicle vehicle = new Vehicle(vehicleMake, vehicleModel, vehicleYear,
+                    vehicleMileage, vehicleName, vehicleAmount);
+            vehicleViewModel.insertVehicle(vehicle);
+            Toast.makeText(getActivity(), "Vehicle Saved", Toast.LENGTH_SHORT).show();
 
-                if (id == -1) {
-                    Toast.makeText(getActivity(), "Vehicle can't be updated", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                String vehicleName = data.getStringExtra(AddVehicleActivity.EXTRA_NAME);
-                String vehicleMake = data.getStringExtra(AddVehicleActivity.EXTRA_MAKE);
-                String vehicleModel = data.getStringExtra(AddVehicleActivity.EXTRA_MODEL);
-                int vehicleYear = data.getIntExtra(AddVehicleActivity.EXTRA_YEAR, 1999);
-                int vehicleMileage = data.getIntExtra(AddVehicleActivity.EXTRA_MILEAGE, 0);
-                double vehicleAmount = data.getDoubleExtra(AddVehicleActivity.EXTRA_AMOUNT, 0.00);
-
-                Calendar calYear = new GregorianCalendar(vehicleYear, 1, 1);
-
-                Vehicle vehicle = new Vehicle(vehicleMake, vehicleModel, new Date(calYear.getTimeInMillis()),
-                        vehicleMileage, vehicleName, vehicleAmount);
-                vehicle.setId(id);
-                vehicleViewModel.updateVehicle(vehicle);
-
-                Toast.makeText(getActivity(), "Vehicle updated", Toast.LENGTH_SHORT).show();
+        } else if(requestCode== EDIT_VEHICLE_REQUEST && resultCode == RESULT_OK){
+            int id = data.getIntExtra(AddEditVehicleActivity.EXTRA_ID, -1);
+            if (id == -1) {
+                Toast.makeText(getActivity(), "Vehicle can't be updated", Toast.LENGTH_SHORT).show();
+                return;
             }
+            String vehicleName = data.getStringExtra(AddEditVehicleActivity.EXTRA_NAME);
+            String vehicleMake = data.getStringExtra(AddEditVehicleActivity.EXTRA_MAKE);
+            String vehicleModel = data.getStringExtra(AddEditVehicleActivity.EXTRA_MODEL);
+            int vehicleYear = data.getIntExtra(AddEditVehicleActivity.EXTRA_YEAR, 1999);
+            int vehicleMileage = data.getIntExtra(AddEditVehicleActivity.EXTRA_MILEAGE, 0);
+
+            //verify
+            double vehicleAmount = data.getDoubleExtra(AddEditVehicleActivity.EXTRA_AMOUNT, 0.00);
+
+            // for the Year date
+            //Calendar calYear = new GregorianCalendar(vehicleYear, 1, 1);
+
+            Vehicle vehicle = new Vehicle(vehicleMake, vehicleModel, vehicleYear,
+                        vehicleMileage, vehicleName, vehicleAmount);
+            vehicle.setId(id);
+            vehicleViewModel.updateVehicle(vehicle);
+
+            Toast.makeText(getActivity(), "Vehicle updated", Toast.LENGTH_SHORT).show();
+
         } else {
             Toast.makeText(getActivity(), "Vehicle not saved", Toast.LENGTH_SHORT).show();
         }
